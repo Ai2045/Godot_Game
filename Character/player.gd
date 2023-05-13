b587ae739e52b5f536bc57cb91b69552880fe90c
Ai2045 <painfohu@gmail.com>
2023-05-13 02:04:55 +0200

    logica player

---
+++ Character/player.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+@export var speed: float = 300.0
+@export var jump_velocity: float = -150.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_vector("left", "right", "up", "down")
+	if direction:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	move_and_slide()
+	update_animation()
+	update_facing_direction()
+
+func update_animation():
+	if is_on_floor():
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else :
+			animated_sprite.play("idle")
+	else :
+		animated_sprite.play("jump_loop")
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+		
+	
